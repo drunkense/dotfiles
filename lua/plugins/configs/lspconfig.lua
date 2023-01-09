cb7c5b646788f44c6044678fbb9646a227b2d3f6
drunkense <drunkense8888@gmail.com>
2023-01-09 18:17:33 +0700

    cssls lsp

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -89,6 +89,11 @@ nvim_lsp.html.setup {
   capabilities = capabilities,
 }
 
+nvim_lsp.cssls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
 nvim_lsp.sumneko_lua.setup {
   on_attach = function(client, bufnr)
     on_attach(client, bufnr)
