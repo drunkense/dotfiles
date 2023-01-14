c9fcef31c8507e245a26695eb2b0bbac7508fb33
drunkense <drunkense8888@gmail.com>
2023-01-14 22:17:07 +0700

    change size toggleterm

--- lua/plugins/configs/toggleterm-nvim.lua
+++ lua/plugins/configs/toggleterm-nvim.lua
@@ -1,7 +1,7 @@
 local status, toggleterm = pcall(require, "toggleterm")
 if (not status) then return end
 require("toggleterm").setup {
-  size = 6,
+  size = 15,
   open_mapping = [[<c-\>]],
   shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
   shading_factor = 1, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
