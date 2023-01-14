16792186f380f9c275190025df6934d2bc0f8f9e
drunkense <drunkense8888@gmail.com>
2023-01-14 22:17:42 +0700

    new config lspsaga

--- lua/plugins/configs/lspsaga.lua
+++ lua/plugins/configs/lspsaga.lua
@@ -1,44 +1,52 @@
---lsp saga
 local status, saga = pcall(require, "lspsaga")
 if not status then
   return
 end
-
-saga.init_lsp_saga {
-  -- use emoji lightbulb in default
+saga.setup { -- defaults ...
+  debug = false,
+  use_saga_diagnostic_sign = true,
+  -- diagnostic sign
+  error_sign = "",
+  warn_sign = "",
+  hint_sign = "",
+  infor_sign = "",
+  diagnostic_header_icon = "   ",
+  -- code action title icon
   code_action_icon = "💡",
-  -- finder icons
-  finder_icons = {
-    def = '  ',
-    ref = '諭 ',
-    link = '  ',
+  code_action_prompt = {
+    enable = true,
+    sign = true,
+    sign_priority = 40,
+    virtual_text = true,
   },
-  -- finder do lsp request timeout
-  -- if your project is big enough or your server very slow
-  -- you may need to increase this value
-  finder_request_timeout = 1500,
-  definition_action_keys = {
-    quit = 'q',
+  finder_definition_icon = "  ",
+  finder_reference_icon = "  ",
+  max_preview_lines = 10,
+  finder_action_keys = {
+    open = "<enter>",
+    vsplit = "s",
+    split = "i",
+    quit = "q",
+    scroll_down = "<C-d>",
+    scroll_up = "<C-u>",
   },
-  rename_action_quit = '<C-q>',
-  rename_in_select = true,
-  -- show outline
-  show_outline = {
-    win_position = 'right',
-    --set special filetype win that outline window split.like NvimTree neotree
-    -- defx, db_ui
-    win_with = '',
-    win_width = 30,
-    auto_enter = true,
-    auto_preview = true,
-    virt_text = '┃',
-    jump_key = 'o',
-    -- auto refresh when change buffer
-    auto_refresh = true,
+  code_action_keys = {
+    quit = "q",
+    exec = "<CR>",
   },
-  server_filetype_map = {
-    typescript = 'typescript',
-    jdtls = 'jdtls',
-    java = 'java',
+  rename_action_keys = {
+    quit = "<C-q>",
+    exec = "<CR>",
   },
+  definition_preview_icon = "  ",
+  border_style = "single",
+  rename_prompt_prefix = "➤",
+  rename_output_qflist = {
+    enable = false,
+    auto_open_qflist = false,
+    in_select = true,
+  },
+  diagnostic_prefix_format = "%d. ",
+  diagnostic_message_format = "%m %c",
+  highlight_prefix = false,
 }
