74f2ecd9ffa27cf78b86a0ce5a51bc42eeecbf99
drunkense <drunkense8888@gmail.com>
2023-01-17 02:24:16 +0700

    enable lsp_saga

--- lua/plugins/configs/lspsaga.lua
+++ lua/plugins/configs/lspsaga.lua
@@ -1,52 +1,52 @@
--- local status, saga = pcall(require, "lspsaga")
--- if not status then
---   return
--- end
--- saga.setup { -- defaults ...
---   debug = false,
---   use_saga_diagnostic_sign = true,
---   -- diagnostic sign
---   error_sign = "",
---   warn_sign = "",
---   hint_sign = "",
---   infor_sign = "",
---   diagnostic_header_icon = "   ",
---   -- code action title icon
---   code_action_icon = "💡",
---   code_action_prompt = {
---     enable = true,
---     sign = true,
---     sign_priority = 40,
---     virtual_text = true,
---   },
---   finder_definition_icon = "  ",
---   finder_reference_icon = "  ",
---   max_preview_lines = 10,
---   finder_action_keys = {
---     open = "<enter>",
---     vsplit = "s",
---     split = "i",
---     quit = "q",
---     scroll_down = "<C-d>",
---     scroll_up = "<C-u>",
---   },
---   code_action_keys = {
---     quit = "q",
---     exec = "<CR>",
---   },
---   rename_action_keys = {
---     quit = "<C-q>",
---     exec = "<CR>",
---   },
---   definition_preview_icon = "  ",
---   border_style = "single",
---   rename_prompt_prefix = "➤",
---   rename_output_qflist = {
---     enable = false,
---     auto_open_qflist = false,
---     in_select = true,
---   },
---   diagnostic_prefix_format = "%d. ",
---   diagnostic_message_format = "%m %c",
---   highlight_prefix = false,
--- }
+local status, saga = pcall(require, "lspsaga")
+if not status then
+  return
+end
+saga.setup { -- defaults ...
+  debug = false,
+  use_saga_diagnostic_sign = true,
+  -- diagnostic sign
+  error_sign = "",
+  warn_sign = "",
+  hint_sign = "",
+  infor_sign = "",
+  diagnostic_header_icon = "   ",
+  -- code action title icon
+  code_action_icon = "💡",
+  code_action_prompt = {
+    enable = true,
+    sign = true,
+    sign_priority = 40,
+    virtual_text = true,
+  },
+  finder_definition_icon = "  ",
+  finder_reference_icon = "  ",
+  max_preview_lines = 10,
+  finder_action_keys = {
+    open = "<enter>",
+    vsplit = "s",
+    split = "i",
+    quit = "q",
+    scroll_down = "<C-d>",
+    scroll_up = "<C-u>",
+  },
+  code_action_keys = {
+    quit = "q",
+    exec = "<CR>",
+  },
+  rename_action_keys = {
+    quit = "<C-q>",
+    exec = "<CR>",
+  },
+  definition_preview_icon = "  ",
+  border_style = "single",
+  rename_prompt_prefix = "➤",
+  rename_output_qflist = {
+    enable = false,
+    auto_open_qflist = false,
+    in_select = true,
+  },
+  diagnostic_prefix_format = "%d. ",
+  diagnostic_message_format = "%m %c",
+  highlight_prefix = false,
+}
